43f3dbca9662ad081fa88c912a9a10047de7abf7
gauravThakur-sgl <gaurav.thakur@shipglobal.in>
2024-10-03 12:12:56 +0530

    Initialized new Project using the react and tailwind css using vite

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-3xl font-bold underline">Hello</h1>
+      </div>
+    </>
+  )
+}
